7fd6f3b9d9034e3b97e381c59de1483a05a850b6
MarkHW <lolka-pop@rambler.ru>
2022-10-27 20:28:43 +0300

    'final'

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,16 @@ const guestScore = document.querySelector('.score-table__score--guest');
 
 function winner() {
     if (homeScore.innerHTML > guestScore.innerHTML) {
-        homeScore.classList.add('score-table__score--winner')
-        guestScore.classList.remove('score-table__score--winner')
+        homeScore.classList.add('score-table__score--winner'),
+        guestScore.classList.remove('score-table__score--winner');
     }
     else {
-        guestScore.classList.add('score-table__score--winner')
-        homeScore.classList.remove('score-table__score--winner')
+        guestScore.classList.add('score-table__score--winner'),
+        homeScore.classList.remove('score-table__score--winner');
+    }
+    if (homeScore.innerHTML == guestScore.innerHTML) {
+        homeScore.classList.remove('score-table__score--winner'),
+        guestScore.classList.remove('score-table__score--winner');
     }
 }
 
@@ -46,6 +50,4 @@ newGame.addEventListener("click", function () {
         isNewGame = true;
     });
 });
-
-
 
